4a70c81e7350c000a7377f2aa80edad4a65e3d86
syahiddraakness <syahidselaluspd708@gmail.com>
2024-02-03 11:03:22 +0700

    kernel: sched: Batter Responsiveness Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1955,7 +1955,7 @@ extern unsigned long sched_get_rt_rq_util(int cpu);
 static __always_inline
 unsigned long arch_scale_freq_capacity(struct sched_domain *sd, int cpu)
 {
-	return SCHED_CAPACITY_SCALE;
+	return capacity_orig_of(cpu);
 }
 #endif
 
@@ -1963,7 +1963,7 @@ unsigned long arch_scale_freq_capacity(struct sched_domain *sd, int cpu)
 static __always_inline
 unsigned long arch_scale_max_freq_capacity(struct sched_domain *sd, int cpu)
 {
-	return SCHED_CAPACITY_SCALE;
+	return capacity_orig_of(cpu);
 }
 #endif
 
@@ -1974,7 +1974,7 @@ unsigned long arch_scale_cpu_capacity(struct sched_domain *sd, int cpu)
 	if (sd && (sd->flags & SD_SHARE_CPUCAPACITY) && (sd->span_weight > 1))
 		return sd->smt_gain / sd->span_weight;
 
-	return SCHED_CAPACITY_SCALE;
+	return capacity_orig_of(cpu);
 }
 #endif
 
@@ -3093,7 +3093,7 @@ static inline void update_cpu_cluster_capacity(const cpumask_t *cpus) { }
 #ifdef CONFIG_SMP
 static inline unsigned long thermal_cap(int cpu)
 {
-	return cpu_rq(cpu)->cpu_capacity_orig;
+	return capacity_orig_of(cpu);
 }
 #endif
 
