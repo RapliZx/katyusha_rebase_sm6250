3b78bc5f2d00fab0881ee70376b24c92c7145fe8
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-02-16 13:47:37 +0800

    devfreq/simple_gpu_algorithm: tune
    enable by def i guess

--- drivers/devfreq/simple_gpu_algorithm.c
+++ drivers/devfreq/simple_gpu_algorithm.c
@@ -23,7 +23,7 @@
 static int default_laziness = 3;
 module_param_named(simple_laziness, default_laziness, int, 0664);
 
-static int ramp_up_threshold = 6000;
+static int ramp_up_threshold = 3000;
 module_param_named(simple_ramp_threshold, ramp_up_threshold, int, 0664);
 
 int simple_gpu_active = 1;
