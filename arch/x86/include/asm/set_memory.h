fdcec68c8c31ee6f605a6ac8b1670ece9ba38150
Rapli Kunz <raffikun1203@gmail.com>
2025-05-11 20:58:40 +0000

    idk

--- arch/x86/include/asm/set_memory.h
+++ arch/x86/include/asm/set_memory.h
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 /* SPDX-License-Identifier: GPL-2.0 */
-=======
->>>>>>> e2927d8994a5 (treewide: move set_memory_* functions away from cacheflush.h)
 #ifndef _ASM_X86_SET_MEMORY_H
 #define _ASM_X86_SET_MEMORY_H
 
@@ -15,10 +12,7 @@
  * Executability : eXeutable, NoteXecutable
  * Read/Write    : ReadOnly, ReadWrite
  * Presence      : NotPresent
-<<<<<<< HEAD
  * Encryption    : Encrypted, Decrypted
-=======
->>>>>>> e2927d8994a5 (treewide: move set_memory_* functions away from cacheflush.h)
  *
  * Within a category, the attributes are mutually exclusive.
  *
