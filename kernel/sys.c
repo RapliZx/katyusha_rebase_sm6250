a5ae6716cfd44ca9288888cda42d53450a39766f
Rapli Kunz <raffikun1203@gmail.com>
2025-05-11 19:00:29 +0000

    Revert "sys_newuname: only spoof for root"
    This reverts commit c7b3a896fbc2610045ae078b9f66aa04c28ade9f.

--- kernel/sys.c
+++ kernel/sys.c
@@ -1214,12 +1214,11 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (current_uid().val == 0 && 
-		(!strncmp(current->comm, "bpfloader", 9) ||
-		!strncmp(current->comm, "netbpfload", 10) ||
-	    	!strncmp(current->comm, "netd", 4))) {
-		strcpy(tmp.release, "5.4.186");
-		pr_info("fake uname: %s/%d release=%s\n",
+	if (!strncmp(current->comm, "netbpfload", 10) &&
+	    current->pid != netbpfload_pid) {
+		netbpfload_pid = current->pid;
+		strcpy(tmp.release, "6.6.40");
+		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
 	// make sure bpf uname spoof is prioritized
