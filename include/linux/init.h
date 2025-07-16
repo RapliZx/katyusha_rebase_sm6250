51d9554ae7c276fe867797b9544daaa9046c58d5
Rapli Kunz <raffikun1203@gmail.com>
2025-07-16 19:34:31 +0000

    Revert "Update CommentBruh"
    This reverts commit 7b75c36a67dfa72b574ec7a25ae73ac11bb0b514.

--- include/linux/init.h
+++ include/linux/init.h
@@ -308,7 +308,7 @@ void __init parse_early_options(char *cmdline);
 #define __exit_p(x) NULL
 #endif
 
-/* KSU (SUKISU) Fix When Compiling Kernel */
+/* KSU FIX IDK ITS WORK OR NOT */
 #define CMD_SUSFS_MOUNT_FOR_ZYGOTE_ISO_SERVICE   0x04
 #define CMD_SUSFS_UMOUNT_FOR_ZYGOTE_ISO_SERVICE  0x05
 
