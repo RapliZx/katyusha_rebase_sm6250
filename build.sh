be8794e50a27477506d04950ca28c5f5c34d3bd9
RapliZx <157944263+RapliZx@users.noreply.github.com>
2025-10-28 21:12:14 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -64,6 +64,9 @@ sleep 1
 echo
 echo -e "\n$green[!] Lets's Build UwU...\033[0m \n"
 sleep 1
+echo -e "\n$blue[+] Running SukiSU-Ultra setup script...\033[0m\n"
+curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-main
+echo -e "\n$green[+] SukiSU-Ultra setup selesai!\033[0m\n"
 fi
 }
 
