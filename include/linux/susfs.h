96176d6648c50ddac38e42a9ef11bd963a4013ba
RapliVx <raffikun1203@gmail.com>
2025-10-28 23:07:28 +0700

    Revert "Fix SukiSU Error"
    This reverts commit 9370e3bc77723728d7caecfd47da81ef09a99efb.

--- include/linux/susfs.h
+++ include/linux/susfs.h
@@ -196,9 +196,6 @@ int susfs_sus_su(struct st_sus_su* __user user_info);
 int susfs_get_enabled_features(char __user* buf, size_t bufsize);
 void susfs_set_avc_log_spoofing(bool enabled);
 
-#define CMD_SUSFS_ADD_SUS_PATH_LOOP 0x1234
-#define TIF_PROC_UMOUNTED    29
-
 /* susfs_init */
 void susfs_init(void);
 
