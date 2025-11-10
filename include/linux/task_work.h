a69f1db3efd2519d43e4de803afa204149c85063
Rapli <154667713+RapliVx@users.noreply.github.com>
2025-11-10 09:52:26 +0700

    Add fallback definitions for task work flags and functions

--- include/linux/task_work.h
+++ include/linux/task_work.h
@@ -22,8 +22,20 @@ static inline void exit_task_work(struct task_struct *task)
 	task_work_run();
 }
 
+/* Fallback untuk task_work flag */
 #ifndef TWA_RESUME
 #define TWA_RESUME false
+#endif
+
+/* Fallback untuk file_operations->fadvise */
+#ifndef HAVE_F_OP_FADVISE
+#define HAVE_F_OP_FADVISE 0
+#endif
+
+/* Fallback untuk copy_file_range (belum ada di kernel 4.14) */
+#ifndef HAVE_COPY_FILE_RANGE
+#define HAVE_COPY_FILE_RANGE 0
+#define ksu_wrapper_copy_file_range ksu_wrapper_clone_file_range
 #endif
 
 #endif	/* _LINUX_TASK_WORK_H */
